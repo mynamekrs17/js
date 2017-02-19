546c9f1c52385d41146df685f9cedc51654f53a5
mynamekrs17 <mynamekrs17@gmail.com>
2017-02-19 15:13:54 +0700

    Update multi-link2.js

--- multi-link2.js
+++ multi-link2.js
@@ -34,11 +34,11 @@ if(x=="12"){xxx="/2016/01/web-hosting-service.html"}
 if(x=="13"){xxx="/2016/01/make-money-in-forex-for-beginners.html"}
 if(x=="14"){xxx="/2016/01/insurance-across-world.html"}
 if(x=="15"){xxx="/2016/01/one-minute-solutions-to-improving-your.html"}
-if(x=="16"){xxx="/2016/01/stress-eating-helps-when-they.html"}
-if(x=="17"){xxx="/2016/01/google-drive-cloud.html"}
-if(x=="18"){xxx="/2016/01/how-to-read-forex-chart-properly.html"}
-if(x=="19"){xxx="/2016/01/auto-insurance-quotes-2015.html"}
-if(x=="20"){xxx="/2016/01/ways-to-get-cheapest-health-insuranc.html"}a_to_ve[i].href="http://converter-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="16"){xxx="/2017/02/stress-eating-helps-when-they.html"}
+if(x=="17"){xxx="/2017/02/google-drive-cloud.html"}
+if(x=="18"){xxx="/2017/02/how-to-read-forex-chart-properly.html"}
+if(x=="19"){xxx="/2017/02/auto-insurance-quotes-2015.html"}
+if(x=="20"){xxx="/2017/02/ways-to-get-cheapest-health-insurance.html"}a_to_ve[i].href="http://converter-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
